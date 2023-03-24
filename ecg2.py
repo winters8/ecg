de44b0117862a9977768350c962dd88fc03bde17
david <arriero.david@gmail.com>
2023-03-24 01:54:37 +0100

    borrado imput

--- ecg2.py
+++ ecg2.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import ecg_plot
 
 plt.rcParams["figure.figsize"] = [7.50, 3.50]
 plt.rcParams["figure.autolayout"] = True
 
 headers = ['I','II','III','aVR','aVL','aVF','V1','V2','V3','V4','V5','V6']
 
-df = pd.read_csv('prueba1.csv', usecols = ['II'])
+df = pd.read_csv('prueba1.csv')
 
-df.set_index('Name').plot()
-
-plt.show()
+ecg_plot.plot(df, sample_rate=500, title = 'ECG')
+ecg_plot.show()
