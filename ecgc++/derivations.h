936ed0d0e6753191e5af7010639aa8fbac8a9686
winters8 <arriero.david@gmail.com>
2023-06-17 23:26:06 +0200

    beguin code

---
+++ ecgc++/derivations.h
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <list>
+#include <string>
+
+class Derivation {
+private:
+    std::list<double> derivation_data;
+    std::string derivation_name;
+
+public:
+    // Constructor derivation
+    Derivation(const std::list<double>& doubleList, const std::string& str) {
+        derivation_data = doubleList;
+        derivation_name = str;
+    }
+    
+    // Getter derivation data
+    std::list<double> getIntList() const {
+        return derivation_data;
+    }
+
+    // Getter name derivation
+    std::string getString() const {
+        return derivation_name;
+    }
+
+    // Setter data derivation
+    void setIntList(const std::list<double>& doubleList) {
+        derivation_data = doubleList;
+    }
+
+    // Setter name derivation
+    void setString(const std::string& str) {
+        derivation_name = str;
+    }
+};
